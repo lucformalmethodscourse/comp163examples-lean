7d400dbbd241d453785661aca5e3b3d791b62e96
Konstantin Läufer <laufer@cs.luc.edu>
2025-11-09 12:57:33 -0600

    added second example on p12
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/week7p12.lean
+++ src/week7p12.lean
@@ -1,4 +1,5 @@
-import Mathlib.Tactic.Ring
+import Mathlib.Data.Real.Basic
+import Mathlib.Tactic.Linarith
 
 
 -- TaiNing's slides week 7 page 12
@@ -31,6 +32,28 @@ example (x : ℕ) : Even (x ^ 2) → Even x := by
   -- done
   contradiction
 
--- TODO second example
+-- YHOO second example
+
+example (a : ℝ) : a ^ 2 ≥ 7 * a → a ≤ 0 ∨ a ≥ 7 := by
+  -- assume antecedent
+  intro h
+  -- assume opposite of consequent to derive a contradiction
+  by_contra hcon
+  -- push negation inside
+  push_neg at hcon
+  -- now we can simply use this one tactic and we're done:
+  -- nlinarith
+  -- OR step-by-step (a bit tedious)
+  rcases hcon with ⟨ha, hb⟩
+  -- rewrite square as multiplication
+  rw [sq] at h
+  -- cancel right multiplication by a
+  have hc : a ≥ 7 := le_of_mul_le_mul_right h ha
+  -- flip inequality
+  rw [ge_iff_le] at hc
+  -- rewrite as negation
+  rw [← not_lt] at hc
+  contradiction
+
 
 -- TODO third example
